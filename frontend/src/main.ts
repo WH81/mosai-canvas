3fbe34268933460e8ea745ce359b2dc83839f091
WH81 <wh81@example.com>
2025-03-14 20:37:13 -0400

    Code updates.

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -13,18 +13,3 @@ bootstrapApplication(AppComponent, {
     provideHttpClient(withInterceptorsFromDi()) // <-- Add this line to provide HttpClient globally
   ],
 });
-
-
-// import { bootstrapApplication } from '@angular/platform-browser';
-// import { provideAnimations } from '@angular/platform-browser/animations';
-// import { provideRouter } from '@angular/router';
-// import { AppComponent } from './app/app.component';
-// import { routes } from './app/app.routes';
-// import 'hammerjs';
-
-// bootstrapApplication(AppComponent, {
-//   providers: [
-//     provideAnimations(),
-//     provideRouter(routes)
-//   ],
-// });
